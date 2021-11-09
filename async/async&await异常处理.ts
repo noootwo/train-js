99610ea36f6c0dcb98831b6889a6f6fb6bdbad24
Pineapple <1244476905@qq.com>
2021-11-09 23:39:12 +0800

    better try/except for async/await by ts

---
+++ async/async&await异常处理.ts
@@ -0,0 +1,14 @@
+// 优雅的使用ts对async/await进行异常处理
+function to<T, U = Error>(
+  promise: Promise<T>,
+  errorExt?: object
+): Promise<[U, undefined] | [null, T]> {
+  return promise
+    .then<[null, T]>((data: T) => [null, data])
+    .catch<[U, undefined]>((err: U) => {
+      if (errorExt) {
+        Object.assign(err, errorExt);
+      }
+      return [err, undefined];
+    });
+}
